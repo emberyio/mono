04f57a039e316b7997639b76e6fc1bbc28cb8b6e
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-10-04 21:36:30 +0700

    vercel bug

--- apps/web1/pages/index.tsx
+++ apps/web1/pages/index.tsx
@@ -2,18 +2,11 @@ import { Box, Center, Heading, MyButton, VStack } from "ui";
 
 export default function Web() {
   return (
-<<<<<<< HEAD
     <Center minH="100vh">
       <VStack>
         <Heading>Web</Heading>
         <MyButton />
       </VStack>
     </Center>
-=======
-    <div>
-      <h1>Webite appp</h1>
-      <Button />
-    </div>
->>>>>>> a83be80af46cb118de1a4bda39f242cfb3fd2d43
   );
 }
